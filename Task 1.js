913b85895d746a4431f8a1e2e417884efa2fbf6d
Adewale IyanuOluwa Isaac <60206492+Ultra-Tech-code@users.noreply.github.com>
2020-03-26 01:58:31 +0100

    JavaScript Task 1

---
+++ Task 1.js
@@ -0,0 +1,21 @@
+let my_name = "Adewale Iyanuoluwa Isaac";
+
+console.log("My name is: " + my_name);
+
+const courses = ["HTML", "CSS", "JAVASCRIPT", "PYTHON", "NODEJS",];
+
+console.log("My courses are: " + courses);
+
+const data = [];
+
+for (let i=0; i<=200; i++) {
+
+  if (i%2===1) {
+
+    data.push(i);
+
+  }
+
+}
+
+console.log(data);
